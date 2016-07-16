cd9ae9f01cb4b05269bd4eb318c1642fca2df1a9
adamliu84 <adam.liu84@gmail.com>
2016-07-17 01:21:35 +0800

    Testing playing youtube video + gitignore

---
+++ app/src/main/java/com/example/adam/androidtestapplication/LearnToPlayActivity.java
@@ -0,0 +1,46 @@
+package com.example.adam.androidtestapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class LearnToPlayActivity extends YouTubeBaseActivity {
+
+    private Button btnFly;
+    private YouTubePlayerView ytpFly;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_to_play);
+
+        ytpFly = (YouTubePlayerView) findViewById(R.id.ytpFly);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("1koy1H9beek");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("ASDF", youTubeInitializationResult.toString());
+            }
+        };
+
+        btnFly = (Button) findViewById(R.id.btnFly);
+        btnFly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ytpFly.initialize(LearnToPlayAPI.api_key, onInitializedListener);
+            }
+        });
+
+    }
+}
