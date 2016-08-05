b127bcf6bff35fa9d09a9d29188133fb47be6a6c
adamliu84 <adam.liu84@gmail.com>
2016-08-06 01:46:04 +0800

    Download from security server url with Authenticator

--- app/src/main/java/com/example/adam/androidtestapplication/HttpGetUPActivity.java
+++ app/src/main/java/com/example/adam/androidtestapplication/HttpGetUPActivity.java
@@ -14,6 +14,14 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+
 public class HttpGetUPActivity extends AppCompatActivity {
 
     private String[] texts = {"Num1","Num2", "Num3", "Num4", "Num5", "Num6", "Num7", "Num8", "Num9", "Num10"};
@@ -33,29 +41,50 @@ public class HttpGetUPActivity extends AppCompatActivity {
     }
 
 
-    private class DownloadSecurityXmlTask extends AsyncTask<Void, String, Void> {
+    private class DownloadSecurityXmlTask extends AsyncTask<Void, Void, Void> {
 
         private int nCurDownload = 0;
+        private String m_url = LearnToPlayAPI.httpGetUP_url;
+        private String m_username = LearnToPlayAPI.httpGetUP_username;
+        private String m_password = LearnToPlayAPI.httpGetUP_password;
 
         @Override
         protected Void doInBackground(Void... voids) {
-            for (String curText : texts) {
-                publishProgress(curText);
-                try{
-                    Thread.sleep(100);
-                }catch (Exception e){
-                    Log.e("ThreadSleepError", e.getMessage());
+            String responseString = null;
+
+            try {
+                URL url = new URL(m_url);
+                //https://examples.javacodegeeks.com/core-java/net/authenticator/access-password-protected-url-with-authenticator/
+                Authenticator.setDefault(new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication (m_username, m_password.toCharArray());
+                    }
+                });
+                //create the new connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                //set up some things on the connection
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoOutput(true);
+                //and connect!
+                urlConnection.connect();
+                //this will be used in reading the data from the internet
+                InputStream inputStream = urlConnection.getInputStream();
+
+                BufferedReader br = null;
+                StringBuilder sb = new StringBuilder();
+                br = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
                 }
-
+                responseString = sb.toString();
+            } catch (Exception e) {
+                responseString = "";
+                Log.e("FailXmlDownload",e.getMessage());
             }
+            Log.d("SuccessfulXmlDownload", responseString);
+
             return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            Log.d("onProgressUpdate", values[0]);
-            Log.d("onProgressUpdateCount", Integer.toString(++nCurDownload));
-
         }
     }
 }
